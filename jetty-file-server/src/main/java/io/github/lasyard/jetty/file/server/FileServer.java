1356846eaca79ea2d6744cd046b5d399d285769b
Yueguang Jiao <lasyard@yeah.net>
2021-11-22 13:39:56 +0800

    Added jetty-file-server.

---
+++ jetty-file-server/src/main/java/io/github/lasyard/jetty/file/server/FileServer.java
@@ -0,0 +1,23 @@
+package io.github.lasyard.jetty.file.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+public final class FileServer {
+    public static final int PORT = 8080;
+
+    private FileServer() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(PORT);
+
+        ResourceHandler handler = new ResourceHandler();
+        handler.setDirectoriesListed(true);
+        handler.setResourceBase(".");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
